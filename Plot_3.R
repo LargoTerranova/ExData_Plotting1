e5df0bc18cbc8a055deb19df2cb61a7396253b22
largoterranova@gmail.com <largoterranova@gmail.com>
2017-08-18 10:08:37 +0200

    Minor changes

--- Plot_3.R
+++ Plot_3.R
@@ -24,6 +24,6 @@ lines(y=february$Sub_metering_3, x = february$new_time, col="blue")
 
 dev.copy(png, file="plot3.png", height=480, width=480)
 
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = "solid", cex =0.9)
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, lwd = 2.5)
 
 dev.off()
